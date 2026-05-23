2b55d871ba3183df5eba35a9554f9f5485ef34af
lizz <lizong1204@gmail.com>
2026-05-23 14:47:25 +0800

    fix: adjust selector

--- lib/routes/4think/utils.ts
+++ lib/routes/4think/utils.ts
@@ -22,8 +22,8 @@ export const parsePost = (html: string, link: string) => {
     const $ = load(html);
 
     return {
-        title: $('meta[property="og:title"]').attr('content') ?? $('h1.gh-article-title').text().trim(),
-        description: $('article.gh-article .gh-content').html() ?? $('section.gh-content').html() ?? '',
+        title: $('meta[property="og:title"]').attr('content') ?? $('h1.single-title').text().trim() ?? $('h1.gh-article-title').text().trim(),
+        description: $('div.single-content.gh-content.gh-canvas').html() ?? $('div.single-content.gh-content').html() ?? $('section.gh-content').html() ?? '',
         pubDate: $('meta[property="article:published_time"]').attr('content'),
         link,
         author: $('meta[name="twitter:data1"]').attr('content') ?? $('a.gh-author-name').first().text().trim(),
